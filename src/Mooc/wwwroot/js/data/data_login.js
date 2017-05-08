6b0330881ea4349afb1396eb14885d449212ae8e
lost <493999874@qq.com>
2017-05-09 01:41:46 +0800

    Add login page

---
+++ src/Mooc/wwwroot/js/data/data_login.js
@@ -0,0 +1,68 @@
+$('form').validate({
+	rules: {
+		email: {
+			required: true
+		},
+		password: {
+			required: true
+		}
+	},
+	messages: {
+		email: {
+			required: '请输入账号'
+		},
+		password: {
+			required: '请输入密码'
+		}
+	},
+	submitHandler: function(form) {
+		var id  = $(form).find('input[name=email]').val();
+		var pwd = $(form).find('input[name=password]').val();
+
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:5000/api/student/' + id,
+			dataType: 'json',
+			success: function(data) {
+				if (data != undefined && data.password === pwd) {
+					store(id, data.name);
+				} else {
+					$.ajax({
+						type: 'GET',
+						url: 'http://localhost:5000/api/teacher/' + id,
+						dataType: 'json',
+						success: function(response) {
+							if (response != undefined && response.password === pwd) {
+								store(id, response.name);
+							} else {
+								toastr.error('登录失败!');
+							}
+						}
+					})
+				}
+			}
+		})
+	}
+});
+
+function store(m_id, m_name) {
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:5000/api/session',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		data: JSON.stringify({
+			id: m_id,
+			name: m_name
+		}),
+		dataType: 'json',
+		success: function(data) {
+			if (data > 0) {
+				window.location.href = 'index.html';
+			} else {
+				toastr.error('会话异常终止！');
+			}
+		}
+	});
+}
