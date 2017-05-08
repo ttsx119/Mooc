d0ddffd22eb88db756e5e65a5a8e1e577bbdfd19
lost <493999874@qq.com>
2017-05-09 00:01:39 +0800

    Fix index page's bug'

--- src/Mooc/Models/Teacher.cs
+++ src/Mooc/Models/Teacher.cs
@@ -7,13 +7,15 @@ namespace Mooc.Models
 {
     public class Teacher
     {
-        public string ID { get; set; }  //教师ID
-        public string Name { get; set; }   //教师姓名
-        public string Password { get; set; }     //教师密码
-        public string Sex { get; set; }      //性别
-        public string Address { get; set; }    //家庭住址
-        public int Age { get; set; }    //年龄
+        public string ID { get; set; }          //教师ID
+        public string Name { get; set; }        //教师姓名
+        public string Password { get; set; }    //教师密码
+        public string Sex { get; set; }         //性别
+        public string Address { get; set; }     //家庭住址
+        public int Age { get; set; }            //年龄
         public int PhoneNumber { get; set; }    //电话
-        public int PhotoUrl { get; set; }    //照片地址
+        public int PhotoUrl { get; set; }       //照片地址
+        public string Introduction { get; set; } //教师介绍
+        
     }
 }
--- src/Mooc/wwwroot/js/data/data_index.js
+++ src/Mooc/wwwroot/js/data/data_index.js
@@ -76,6 +76,10 @@ function sortByHot(a, b) {
 	return b.studentNumber - a.studentNumber;
 }
 
+function sortByTime(a, b) {
+	return Date.parse(b.uploadTime.replace(/T/, ' ')) - Date.parse(a.uploadTime.replace(/T/, ' '));
+}
+
 // 课程显示
 $.ajax({
 	type: 'GET',
@@ -84,55 +88,137 @@ $.ajax({
 	success: function(data) {
 		data.sort(sortByHot);
 
-		var hotIndex = 0;
-		var hotHTML  = '';
+		var hotIndex 	 = 0;
+		var hotHTML  	 = '';
+		var newHTML		 = '';
+		var teacherName  = '';
+		var teacherIntro = '';
 
-		data.map(function(hotItem) {
-			hotIndex++;
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:5000/api/teacher',
+			dataType: 'json',
+			success: function(teacher) {
+				data.map(function(hotItem) {
+					hotIndex++;
+					var teacherName  = '';
+					var teacherIntro = '';
 
-			if (hotIndex < 17) {
-				if (hotIndex % 2 === 1) {
-					hotHTML += '<li>';
-				}
+					teacher.map(function(singleItem) {
+						if (singleItem.id == hotItem.teacherID) {
+							teacherName  = singleItem.name;
+							teacherIntro = singleItem.introduction;
+						}
+					});
 
-				hotHTML += '<div class="course_item">';
-				hotHTML += '<div class="item_header">';
-				hotHTML += '<a href="lesson.html?id=';
-				hotHTML += hotItem.id;
-				hotHTML += '" target="_parent">';
-				hotHTML += hotItem.name;
-				hotHTML += '</a>';
-				hotHTML += '<span>';
-				hotHTML += hotItem.category === 0 ? '[线上]' : '[线下]';
-				hotHTML += '</span>';
-				hotHTML += '</div>';
-				hotHTML += '<span>';
-				hotHTML += '<span>教师: </span>';
-				hotHTML += hotItem.teacherID;
-				hotHTML += '</span>';
-				hotHTML += '<span>';
-				hotHTML += '<span>时长: </span>';
-				hotHTML += hotItem.duration;
-				hotHTML += '</span>';
-				hotHTML += '<span>';
-				hotHTML += '<span>上课时间: </span>';
-				hotHTML += hotItem.courseTime;
-				hotHTML += '</span>';
-				hotHTML += '<br />';
-				hotHTML += '<span>';
-				hotHTML += '<span>教师优势: </span>';
-				hotHTML += hotItem.duration;
-				hotHTML += '</span>';
-				hotHTML += '</div>';
+					if (hotIndex < 17) {
+						if (hotIndex % 2 === 1) {
+							hotHTML += '<li>';
+						}
 
-				if (hotIndex % 2 === 0) {
-					hotHTML += '</li>';
-				}
+						hotHTML += '<div class="course_item">';
+						hotHTML += '<div class="item_header">';
+						hotHTML += '<a href="lesson.html?id=';
+						hotHTML += hotItem.id;
+						hotHTML += '" target="_parent">';
+						hotHTML += hotItem.name;
+						hotHTML += '</a>';
+						hotHTML += '<span>';
+						hotHTML += hotItem.category === 0 ? '[线上]' : '[线下]';
+						hotHTML += '</span>';
+						hotHTML += '</div>';
+						hotHTML += '<span>';
+						hotHTML += '<span>教师: </span>';
+						hotHTML += teacherName;
+						hotHTML += '</span>';
+						hotHTML += '<span>';
+						hotHTML += '<span>时长: </span>';
+						hotHTML += hotItem.duration;
+						hotHTML += '</span>';
+						hotHTML += '<br />';
+						hotHTML += '<span>';
+						hotHTML += '<span>上课时间: </span>';
+						hotHTML += hotItem.courseTime.replace(/T/, ' ');
+						hotHTML += '</span>';
+						hotHTML += '<br />';
+						hotHTML += '<span>';
+						hotHTML += '<span>教师优势: </span>';
+						hotHTML += teacherIntro;
+						hotHTML += '</span>';
+						hotHTML += '</div>';
+
+						if (hotIndex % 2 === 0) {
+							hotHTML += '</li>';
+						}
+					}
+				});
+
+				hotHTML += '<a href="javascript:void(0);" target="_parent">查看更多</a>';
+				hotIndex = 0;
+
+				$('.tab_body').find('.hot_body').html(hotHTML);
+
+				data.sort(sortByTime);
+
+				data.map(function(dataItem) {
+					hotIndex++;
+					var teacherName  = '';
+					var teacherIntro = '';
+
+					teacher.map(function(singleItem) {
+						if (singleItem.id == dataItem.teacherID) {
+							teacherName  = singleItem.name;
+							teacherIntro = singleItem.introduction;
+						}
+					});
+
+					if (hotIndex < 17) {
+						if (hotIndex % 2 === 1) {
+							newHTML += '<li>';
+						}
+
+						newHTML += '<div class="course_item">';
+						newHTML += '<div class="item_header">';
+						newHTML += '<a href="lesson.html?id=';
+						newHTML += dataItem.id;
+						newHTML += '" target="_parent">';
+						newHTML += dataItem.name;
+						newHTML += '</a>';
+						newHTML += '<span>';
+						newHTML += dataItem.category === 0 ? '[线上]' : '[线下]';
+						newHTML += '</span>';
+						newHTML += '</div>';
+						newHTML += '<span>';
+						newHTML += '<span>教师: </span>';
+						newHTML += teacherName;
+						newHTML += '</span>';
+						newHTML += '<span>';
+						newHTML += '<span>时长: </span>';
+						newHTML += dataItem.duration;
+						newHTML += '</span>';
+						newHTML += '<br />';
+						newHTML += '<span>';
+						newHTML += '<span>上课时间: </span>';
+						newHTML += dataItem.courseTime.replace(/T/, ' ');
+						newHTML += '</span>';
+						newHTML += '<br />';
+						newHTML += '<span>';
+						newHTML += '<span>教师优势: </span>';
+						newHTML += teacherIntro;
+						newHTML += '</span>';
+						newHTML += '</div>';
+
+						if (hotIndex % 2 === 0) {
+							newHTML += '</li>';
+						}
+					}
+				});
+
+				newHTML += '<a href="javascript:void(0);" target="_parent">查看更多</a>';
+				hotIndex = 0;
+
+				$('.tab_body').find('.new_body').html(newHTML);
 			}
 		});
-
-		hotHTML += '<a href="javascript:void(0);" target="_parent">查看更多</a>';
-
-		$('.tab_body').find('ul:first-of-type').html(hotHTML);
 	}
 });
